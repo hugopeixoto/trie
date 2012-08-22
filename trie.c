3b2d09a42b0417c636fbb4c3525ffae8d5662da5
Hugo Peixoto <hugo.peixoto@gmail.com>
2012-08-22 02:21:14 +0100

    Optimized node lookup/insertion in trie_compress.
    Not exactly a hash table or a balanced binary tree,
    but it seems that binary search and an insertion
    sort with memmove is good enough.

--- trie.c
+++ trie.c
@@ -7,7 +7,7 @@
 
 static const struct node* trie_node_lookup (const struct trie*, const struct node*, const char*);
 static void trie_node_insert (struct trie*, struct node*, const char*);
-static struct node* trie_node_compress (struct trie*, struct node*, struct node**);
+static struct node* trie_node_compress (struct trie*, struct node*, struct node**, int* a_used);
 
 struct trie* trie_alloc ()
 {
@@ -82,47 +82,84 @@ static const struct node* trie_node_lookup (const struct trie* a_trie, const str
 
 void trie_compress (struct trie* a_trie)
 {
+  int used = 0;
   struct node** nodes = (struct node**)malloc(sizeof(struct node*) * a_trie->node_count_);
 
   memset(nodes, 0, sizeof(struct node*) * a_trie->node_count_);
 
-  trie_node_compress(a_trie, a_trie->root_, nodes);
+  trie_node_compress(a_trie, a_trie->root_, nodes, &used);
 
   free(nodes);
 }
 
-static int trie_equivalent (const struct node* a_a, const struct node* a_b)
+static struct node** trie_equivalent_binary_search (struct node** a_nodes, struct node* a_node, int* a_used)
 {
-  return memcmp(a_a, a_b, sizeof(*a_a)) == 0;
+  int begin = 0;
+  int end   = *a_used;
+  int p     = 0;
+  int cmp   = 0;
+
+  while (begin != end) {
+    p = begin + (end - begin) / 2;
+    
+    cmp = memcmp(a_node, a_nodes[p], sizeof(*a_node));
+
+    if (cmp == 0) {
+      return a_nodes + p;
+    }
+
+    if (cmp < 0) {
+      end = p;
+    } else {
+      begin = p + 1;
+    }
+  }
+
+  return a_nodes + begin;
 }
 
-static struct node** trie_equivalent_lookup (struct node** a_nodes, struct node* a_node)
+static struct node** trie_equivalent_lookup (struct node** a_nodes, struct node* a_node, int* a_used)
 {
-  while (*a_nodes && !trie_equivalent(a_node, *a_nodes)) ++a_nodes;
+  struct node** p = trie_equivalent_binary_search(a_nodes, a_node, a_used);
 
-  return a_nodes;
+  if (*p && memcmp(*p, a_node, sizeof(*a_node)) == 0) {
+    return p;
+  }
+
+  return NULL;
 }
 
-static struct node* trie_node_compress (struct trie* a_trie, struct node* a_node, struct node** a_nodes)
+
+static void trie_equivalent_insert (struct node** a_nodes, struct node* a_node, int* a_used)
+{
+  struct node** p = trie_equivalent_binary_search(a_nodes, a_node, a_used);
+
+  memmove(p + 1, p, sizeof(struct node*) * (unsigned int)(a_nodes + *a_used - p));
+  *p = a_node;
+  ++*a_used;
+}
+
+static struct node* trie_node_compress (struct trie* a_trie, struct node* a_node, struct node** a_nodes, int* a_used)
 {
   struct node** ptr = NULL;
 
   if (a_node->left) {
-    a_node->left  = trie_node_compress(a_trie, a_node->left, a_nodes);
+    a_node->left  = trie_node_compress(a_trie, a_node->left, a_nodes, a_used);
   }
 
   if (a_node->right) {
-    a_node->right = trie_node_compress(a_trie, a_node->right, a_nodes);
+    a_node->right = trie_node_compress(a_trie, a_node->right, a_nodes, a_used);
   }
 
-  ptr = trie_equivalent_lookup(a_nodes, a_node);
+  ptr = trie_equivalent_lookup(a_nodes, a_node, a_used);
     
-  if (*ptr) {
+  if (ptr) {
     --(a_trie->node_count_);
     node_dealloc(a_node);
     return *ptr;
   } else {
-    return *ptr = a_node;
+    trie_equivalent_insert(a_nodes, a_node, a_used);
+    return a_node;
   }
 }
 
